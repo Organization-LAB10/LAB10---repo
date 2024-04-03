15f3035cec8e187d8e765d3d0a6ed6f204dc8fbd
Admin <Admin@DESKTOP-RI0URUI>
2024-04-03 11:03:50 +0800

    Javhaaa

--- LAB10/src/Laboratory_10.java
+++ LAB10/src/Laboratory_10.java
@@ -4,6 +4,7 @@ public class Laboratory_10 {
 	public static void main(String[] args) {
 		System.out.print("HOWHOHOWHOWHOWHOWHOWHWOOHWOHWHWOW?");
 		System.out.print("HOOOOOW?");
+		System.out.print("javhaa");
 
 	}
 
